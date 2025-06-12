031f050f7a665c716756791bea93a35af2e7658c
goldenplayer5630 <112952889+goldenplayer5630@users.noreply.github.com>
2025-06-13 01:03:47 +0200

    Fixed typo

--- Hourly.Api/CorsSettings.cs
+++ Hourly.Api/CorsSettings.cs
@@ -1,6 +1,6 @@
 ﻿namespace Hourly.Api
 {
-    public class ReactCorsSettings
+    public class CorsSettings
     {
         public string[] AllowedOrigins { get; set; } = [];
         public string[] AllowedMethods { get; set; } = [];
