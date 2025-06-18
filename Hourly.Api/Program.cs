afa9679486629fa15fbdf801ca393c536c4e977b
goldenplayer5630 <112952889+goldenplayer5630@users.noreply.github.com>
2025-06-19 01:25:17 +0200

    t

--- Hourly.Api/Program.cs
+++ Hourly.Api/Program.cs
@@ -58,11 +58,8 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Enable CORS before endpoints ??
 app.UseCors("AllowReactApp");
